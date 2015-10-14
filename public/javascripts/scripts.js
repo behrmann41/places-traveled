156833f069110a1473ea727b20481cce45acb7c2
Ian Behrmann <ianjbehrmann@gmail.com>
2015-10-14 16:03:43 -0600

    -Markers now showing on map when adding / deleting location

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,14 +1,34 @@
 $(document).ready(function(){
+  function initMap() {
+    console.log('hello')
+    // Create a map object and specify the DOM element for display.
+  }
+  var map;
   function loadPlaces() {
     $.getJSON('/places/data', function (data){
         $('#tbody').html('');
+        var locations = []
         for (var i = 0; i < data.length; i++){
           var addr = data[i].address
           var lat = data[i].latitude
           var lon = data[i].longitude
           var placeId = data[i]._id
           $('#tbody').append('<tr><td>' + addr + '</td><td>' + lat + '</td><td>' + lon + '</td><td><button class="delete" rel="'+ placeId +'">X</button></td></tr>')
+          var myLatLng = {lat: lat, lng: lon}
+          locations.push(myLatLng)
         }
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 0, lng: 0},
+            scrollwheel: false,
+            zoom: 2
+        });
+        locations.forEach(function(loc){
+          var marker = new google.maps.Marker({
+            position: loc,
+            map: map,
+            title: 'hi'
+          })
+        })
     })
   }
 
@@ -25,6 +45,13 @@ $(document).ready(function(){
       data: newPlace,
       success: function (data){
         $('#tbody').append('<tr><td>' + data.address + '</td><td>' + data.latitude + '</td><td>' + data.longitude + '</td><td><button class="delete" rel="'+ data._id +'">X</button></td></tr>')
+        var myLatLng = {lat: data.latitude, lng: data.longitude}
+        console.log(myLatLng)
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'hi'
+        })
       },
       error: function (error){
         console.log('error', error)
@@ -51,14 +78,6 @@ $(document).ready(function(){
     }
   })
 
-  function initMap() {
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      scrollwheel: false,
-      zoom: 8
-    });
-  }
-  loadPlaces()
   initMap()
+  loadPlaces()
 })
